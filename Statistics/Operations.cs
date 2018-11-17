06959086459bd705f083165fc9d182aab61515c0
Anastasija Pischik <nastijpischik@gmail.com>
2018-11-17 22:40:07 +0300

    v1

---
+++ Statistics/Operations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.Statistics;
+
+namespace Statistics
+{
+    class Operations
+    {
+        public void Menu(List<double> data)
+        {           
+            int choose = 0;
+            while (choose!=6)
+            {
+                choose = Convert.ToInt16(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine(data.Maximum() + "; " + data.Minimum());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine(data.Mean());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine(data.Median());
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine(data.Variance());
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Input tau [0,0 - 1,0]");
+                            Console.WriteLine(data.Quantile(Convert.ToDouble(Console.ReadLine())));
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Press any key to exit");
+                            return;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Statistics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input size of data");
+            try
+            {
+                int n = Convert.ToInt16(Console.ReadLine());
+                List<double> data = new List<double>();
+                Console.WriteLine("Input data");
+                for (int i = 0; i < n; i++)
+                {
+                    data.Add(Convert.ToDouble(Console.ReadLine()));
+                }
+                Operations oper = new Operations();
+                Console.WriteLine(
+                    "Choose operation:\n" +
+                    "1 Maximum and minimum\n" +
+                    "2 Mean\n" +
+                    "3 Median\n" +
+                    "4 Variance\n" +
+                    "5 Quantile\n" +
+                    "6 Exit"
+                 );
+                oper.Menu(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+            Console.ReadLine();
+
+        }
+    }
+}
